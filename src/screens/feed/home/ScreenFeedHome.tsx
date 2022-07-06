da0ea508ae0f4735c1fbf1cfe219d9da96c57b71
Luis Fernando Miranda <abraxas@abraxas-3.local>
2022-07-05 22:32:18 -0500

    feed screen

--- src/screens/feed/home/ScreenFeedHome.tsx
+++ src/screens/feed/home/ScreenFeedHome.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import PinCard from '../../../components/Cards/PinCard';
-import { styleBackground } from '../../../themes/styles';
+import { styleBackground, styleWrappers } from '../../../themes/styles';
+import { COLORS } from '../../../themes/colors/ZettingColors';
+import MasonrryList from '../../../components/Lists/MasonryList';
+import FeedContainer from '../../../functionalities/feed/containers/FeedContainer/FeedContainer';
+
+
 const ScreenFeedHome = () => {
     return (
-        <SafeAreaView style={styleBackground.darkBackground}>
-            <PinCard />
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.blue }}>
+            <FeedContainer />
         </SafeAreaView >
     );
 };
