8830f40237159a635e5d1b58fba2da880d055286
Luis Fernando Miranda <abraxas@abraxas-3.local>
2022-07-05 19:31:14 -0500

    Props and hook to Pins

--- src/components/Cards/PinCard/PinCard.tsx
+++ src/components/Cards/PinCard/PinCard.tsx
@@ -1,22 +1,38 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
-const PinCard = () => {
+type Props = {
+    pin: {
+        title: string,
+        tag: string,
+        image: string,
+    }
+
+}
+const PinCard = ({ pin }: Props) => {
+    const { title, image, tag } = pin
+    const [ratio, setRatio] = useState(1);
+
+    useEffect(() => {
+        if (image) {
+            Image.getSize(image, (width, height) => setRatio(width / height))
+        }
+    }, [image]);
     return (
         <TouchableOpacity style={styles.pinContainer} activeOpacity={0.9}>
             <View>
-                <Image source={{ uri: 'https://images.unsplash.com/photo-1657030871212-95d863306bed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80' }}
-                    style={styles.image}
+                <Image source={{ uri: image }}
+                    style={[styles.image, { aspectRatio: ratio }]}
                 />
                 <LinearGradient
                     colors={['#051D2E', 'transparent',]}
-                    style={{ height: '100%', width: '100%', borderRadius: 20, position: 'absolute' }}
+                    style={{ height: '100%', aspectRatio: ratio, borderRadius: 20, position: 'absolute' }}
                     start={{ x: 0.3, y: 1.1 }}
                     end={{ x: 0.3, y: 0.4 }}
                 />
-                <Text style={styles.title} >Binoid {'\n'} Product Photography</Text>
+                <Text style={styles.title} >{title} {'\n'} {tag}</Text>
             </View>
         </TouchableOpacity>
     );
--- src/components/Cards/PinCard/styles.tsx
+++ src/components/Cards/PinCard/styles.tsx
@@ -8,7 +8,6 @@ export const styles = StyleSheet.create({
     },
     image: {
         width: '100%',
-        height: 200,
         borderRadius: 20
     },
     title: {
